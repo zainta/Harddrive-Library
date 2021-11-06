182f1f24358bb76716a3e3440a6a76dcb3ebfd77
zainta <zainta@gmail.com>
2021-11-06 12:35:06 -0400

    * Added copyright notice.

--- Harddrive-Library/Web/HDSLWebController.cs
+++ Harddrive-Library/Web/HDSLWebController.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿// Copyright (c) Zain Al-Ahmary.  All rights reserved.
+// Licensed under the MIT License, (the "License"); you may not use this file except in compliance with the License. 
+// You may obtain a copy of the License at https://mit-license.org/
+
+using System;
 using System.Linq;
 using HDDL.Data;
 using HDDL.HDSL;
